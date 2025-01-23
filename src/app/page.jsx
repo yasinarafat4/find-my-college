6d57b98e8ad03abf787f1ebc949caf12bc7b7b7e
yasinarafat4 <mdyasinarafathasib@gmail.com>
2025-01-23 08:00:59 +0600

    Initial Setup Done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Welcom to Find My College!</h1>
     </div>
   );
 }
