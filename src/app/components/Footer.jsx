21183a9e673d0310a2cbf974b75ca86e8b5aa287
yasinarafat4 <mdyasinarafathasib@gmail.com>
2025-02-11 08:04:09 +0600

    Primary setup done

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="bg-slate-800 text-slate-200 p-3 text-center">Copyright &copy; 2025. All Rights Reseved!</div>;
+};
+
+export default Footer;
